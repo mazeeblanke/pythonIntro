1c42e114c14820bc8d7af43e94d2dcdb35906b45
mazino <ewomaukah@yahoo.com>
2020-05-17 20:01:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction python
+
+## just the basics
---
+++ main.py
@@ -0,0 +1,21 @@
+print ('sdjibsidu') # print a value to the console
+
+# for loop
+for i in [2, "list", "jskd sdsd"]:
+    print (i)
+
+# while loop
+i = 0
+
+while (i < 10) :
+    print (i)
+    i += 1
+
+# if control structure
+
+i = 40
+
+if (i == 4):
+    print (i)
+else:
+    print('Error')        
